469b9b4f1953823d0d679cf995b0870c1b075664
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-12 15:27:50 +0530

    E-Commerce App Homepage Design |E-commerce Product List Design | Categories Component

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type ProductType = {
+  id: string | number;
+  name: string;
+  shortDescription: string;
+  description: string;
+  price: number;
+  sizes: string[];
+  colors: string[];
+  images: Record<string, string>;
+};
+
+export type ProductsType = ProductType[]; //actual [] likhana padta hai kyuki ham array types check karenge
+
+//agar export type ProductsType = ProductType[]; export nhi karna h to jha bhi aap import karenge vha
+
+//import { ProductType } from "@/Types";
+// const products: ProductType[] =  aisa likhana padega
