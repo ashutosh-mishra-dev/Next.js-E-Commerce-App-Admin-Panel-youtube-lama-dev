bbf6af269d12cfc633a946ffc881cf4c0093ef43
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-13 14:34:24 +0530

    E-commerce Interactive Product Card Design

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -120,7 +120,7 @@ const ProductList = () => {
     <div className="w-full">
       <Categories />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
+      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
