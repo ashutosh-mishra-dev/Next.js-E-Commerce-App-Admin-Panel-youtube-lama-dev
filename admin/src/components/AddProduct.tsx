fd34759c33b3d7b5a51ed5af20448ff8bb046ea8
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-25 12:23:42 +0530

    feat(admin): E-commerce Admin Add Product Form Design

--- admin/src/components/AddProduct.tsx
+++ admin/src/components/AddProduct.tsx
@@ -22,6 +22,8 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { Checkbox } from "./ui/checkbox";
+import { ScrollArea } from "./ui/scroll-area";
 
 
 const categories = [
@@ -91,6 +93,7 @@ const AddProduct = () => {
   });
   return (
     <SheetContent>
+      <ScrollArea className="h-screen">
       <SheetHeader>
         <SheetTitle className="mb-4">Add Product</SheetTitle>
         <SheetDescription asChild>
@@ -185,11 +188,85 @@ const AddProduct = () => {
                   </FormItem>
                 )}
               />
+              <FormField
+                control={form.control}
+                name="sizes"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Sizes</FormLabel>
+                    <FormControl>
+                      <div className="grid grid-cols-3 gap-4 my-2">
+                        {sizes.map(size=>(
+                          <div className="flex items-center gap-2" key={size}>
+                          <Checkbox id="size" checked={field.value?.includes(size)} onCheckedChange={(checked)=>{
+                            const currentValues = field.value || [];
+
+                            if(checked){
+                              field.onChange([...currentValues,size])
+                            }else{field.onChange(currentValues.filter(v=>v !== size))}
+                          }} />
+                          <label htmlFor="size" className="text-sm">{size }</label>
+                        </div>))}
+                      </div>
+                    </FormControl>
+                    <FormDescription>
+                      Select the available sizes for the product.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              
+              <FormField
+                control={form.control}
+                name="colors"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Colors</FormLabel>
+                    <FormControl>
+                      <div className="space-y-4">
+                      <div className="grid grid-cols-3 gap-4 my-2">
+                        {colors.map(color=>(
+                          <div className="flex items-center gap-2" key={color}>
+                          <Checkbox id="color" checked={field.value?.includes(color)} onCheckedChange={(checked)=>{
+                            const currentValues = field.value || [];
+
+                            if(checked){
+                              field.onChange([...currentValues,color])
+                            }else{field.onChange(currentValues.filter(v=>v !== color))}
+                          }} />
+                          <label htmlFor="color" className="text-sm flex items-center gap-2">
+                            <div className="w-2 h-2 rounded-full" style={{ backgroundColor:color }} />
+                            {color }
+                            </label>
+                        </div>))}
+                      </div>
+                      {field.value && field.value.length > 0 && (
+                        <div className="mt-8 space-y-4">
+                          <p className="text-sm font-medium">Upload images for selected colors : </p>
+                          {field.value.map(color=>(<div className="flex items-center gap-2" key={color}>
+                             <div className="w-2 h-2 rounded-full" style={{ backgroundColor:color }} />
+                            <span className="text-sm min-w-[60px]">{color }</span>
+                            <Input type="file" accept="image/*"/>
+                          </div>))}
+                        </div>
+                      )}
+                      </div>
+                    </FormControl>
+                    <FormDescription>
+                      Select the available colors for the product.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
               <Button type="submit">Submit</Button>
             </form>
           </Form>
         </SheetDescription>
       </SheetHeader>
+      </ScrollArea>
     </SheetContent>
   );
 };
